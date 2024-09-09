367691575003ea61ed0c280d4532f67e81d920fe
CarlosMendes1 <111753284+CarlosMendes1@users.noreply.github.com>
2024-09-09 01:04:56 +0100

    Adicionar arquivos de projeto.

---
+++ MicroCredit.Domain/Entities/MicroCredit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroCredit.Domain.Entities
+{
+    public class MicroCredit
+    {
+        new public string MyProperty { get; set; }
+    }
+}
